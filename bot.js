bc061230620f1e22ea3c83593ef636109f35cb83
mraco <36960271+mraco@users.noreply.github.com>
2018-09-03 16:06:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -121,7 +121,7 @@ client.on('message', async msg => { // eslint disable line
                 try {
                     var videos = await youtube.searchVideos(searchString, 10);
                     let index = 0;
-                    msg.channel.send(`${videos.map(video2 => `\`${++index}\` **-** ${video2.title}`).join('\n')}`).then(message =>{message.delete(20000)})
+                    msg.channel.send(`**${videos.map(video2 => `\`${++index}\` **-** ${video2.title}`).join('\n')}**`).then(message =>{message.delete(20000)})
                         
                    .then(message =>{message.delete(20000)})
                     // eslint-disable-next-line max-depth
