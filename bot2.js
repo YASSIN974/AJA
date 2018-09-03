d6963915a0c794f43a53f225d170672f6e1b8ac2
mraco <36960271+mraco@users.noreply.github.com>
2018-09-03 14:36:50 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -7,7 +7,7 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
  
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = 'a';
+const prefix = 'y';
 const discord_token = process.env.BOT_TOKEN;
 
 client.on('ready', function() {
