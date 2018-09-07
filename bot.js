359633fe7e55aa4e6244d556e95649e93419e549
mraco <36960271+mraco@users.noreply.github.com>
2018-09-07 20:18:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ LET'S GO!
   client.user.setStatus("dnd");
 
 });
-
+/*
 client.on('ready', function(){
     client.user.setStatus("dnd");
     var ms = 10000 ;
@@ -48,7 +48,30 @@ client.on('ready', function(){
     }, ms);10000
 
 });
+*/
+client.on('ready', function(){
+ console.log(`Logged in as ${client.user.tag}!`);
+    
+   // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
+   var s = ['483055660209012736','480169573530861578','483055655800930315'];
+    setInterval(function (){
+    client.user.setPresence({
+ game: { 
+    type: 1,
+     url: 'https://www.twitch.tv/skwadraa',
+    name: '...',
+    application_id: '477187715658547201',
+     assets: {
+         large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
+  
+    }
 
+  }
+    });
+
+    }, 5000);
+
+});
 client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
 
 client.on('reconnecting', () => console.log('I am reconnecting now!'));
