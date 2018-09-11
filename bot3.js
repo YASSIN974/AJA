04f8e0e9077667cf4ec2b006e1c4cb52ea224201
mraco <36960271+mraco@users.noreply.github.com>
2018-09-11 21:44:34 +0100

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const bot = new Discord.Client();
+const mtra9a = new Discord.Client();
 client.on("ready", () => {
 console.log('hello again bitch');
  console.log(`Logged in as ${client.user.tag}!`);
@@ -24,5 +25,13 @@ let channel =     bot.channels.get("489069924736565268")
 });
 });
 
+mtra9a.on("ready", () => {
+let channel =     mtra9a.channels.get("489069924736565268")
+     setInterval(() => {
+    channel.send(Math.random().toString(20).substring(1))
+
+});
+});
 client.login(process.env.BOT_TOKEN3);
 bot.login(process.env.BOT_TOKEN4);
+mtra9a.login(process.env.BOT_TOKEN5);
