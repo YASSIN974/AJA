3b5f0e42aa036df410c2492164fb9e7629c623cb
mraco <36960271+mraco@users.noreply.github.com>
2018-09-11 16:15:19 +0100

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -8,26 +8,21 @@ console.log('hello again bitch');
 
 client.on("ready", () => {
 
-let channel = client.channels.get("485459134175641613"); 
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 100000; x++) {
+let channel = client.channels.get("489074364512337925"); 
+       setInterval(() => {
 channel.send(Math.random().toString(36).substring(7))
 
-        }
+     
       
+},10);
 });
 bot.on("ready", () => {
-let channel =     bot.channels.get("484762716091842572")
-    
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 100000; x++) {
-channel.send(Math.random().toString(36).substring(7))
+let channel =     bot.channels.get("489069924736565268")
+     setInterval(() => {
+    channel.send(Math.random().toString(36).substring(5))
 
-
-        }
-      
 });
+});
+
 client.login(process.env.BOT_TOKEN3);
 bot.login(process.env.BOT_TOKEN4);
